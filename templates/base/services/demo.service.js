18e485bd0a90a6a4fad3e0a9d14cadaa0bda6161
siwilizhao <siwilizhao@gmail.com>
2019-09-05 00:43:05 +0800

    base template add demo service

---
+++ templates/base/services/demo.service.js
@@ -0,0 +1,12 @@
+/**
+ * @author [siwilizhao]
+ * @email [siwilizhao@gmail.com]
+ * @create date 2019-09-05 00:41:52
+ * @modify date 2019-09-05 00:41:52
+ * @desc [demo service]
+ */
+
+ class DemoService {
+
+ }
+ module.exports = DemoService
