f742cae23896c8ad6c1ec3be2f81c52e113c09fe
siwilizhao <siwilizhao@gmail.com>
2019-09-05 00:32:29 +0800

    add deploy.script.js

---
+++ scripts/deploy.script.js
@@ -0,0 +1,11 @@
+/**
+ * @author [siwilizhao]
+ * @email [siwilizhao@gmail.com]
+ * @create date 2019-09-05 00:12:51
+ * @modify date 2019-09-05 00:12:51
+ * @desc [deploy script]
+ */
+
+;(async () => {
+
+})();
