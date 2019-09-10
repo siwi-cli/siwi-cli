7e2c464b09939fde555e2c1a03e400cbe88b9f6d
siwilizhao <siwilizhao@gmail.com>
2019-09-10 09:05:08 +0800

    fix

--- cli/custom.cli.js
+++ cli/custom.cli.js
@@ -10,12 +10,34 @@ const fs = require('fs')
 const path = require('path')
 const homedir = os.homedir()
 const DOT_SIWI_PATH = path.resolve(homedir, '.siwi')
-console.log(DOT_SIWI_PATH);
+const CUSTOM_TEMPLATES_PATH = path.resolve(DOT_SIWI_PATH, 'templates')
+const CUSTOM_CONFIG_PATH = path.resolve(DOT_SIWI_PATH, 'config')
+
 
 class CustomCli {
+    constructor () {
+
+    }
+    
+    /**
+     * init
+     *
+     * @author (siwilizhao<siwilizhao@gmail.com>)
+     * @returns
+     * @memberof CustomCli
+     */
     async init() {
         if (!fs.existsSync(DOT_SIWI_PATH)) {
             fs.mkdirSync(DOT_SIWI_PATH)
+            console.log(DOT_SIWI_PATH)
+        }
+        if (!fs.existsSync(CUSTOM_TEMPLATES_PATH)) {
+            fs.mkdirSync(CUSTOM_TEMPLATES_PATH)
+            console.log(CUSTOM_TEMPLATES_PATH)
+        }
+        if (!fs.existsSync(CUSTOM_CONFIG_PATH)) {
+            fs.mkdirSync(CUSTOM_CONFIG_PATH)
+            console.log(CUSTOM_CONFIG_PATH)
         }
         return true
     }
